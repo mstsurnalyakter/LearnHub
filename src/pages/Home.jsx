57432df5572b6cb3492b6b58416e6f376ac65ab3
mstsurnalyakter <surnalyms@gmail.com>
2024-03-22 00:37:15 +0600

    home page completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
+import wave from "../assets/wave.svg"
+import Hero from "../components/Hero";
 const Home = () => {
   return (
-    <div className="container  mx-auto px-7 lg:px-5 xl:px-7 min-h-[calc(100vh-310px)]">
-      <h1>Home</h1>
+    <div className="min-h-[calc(100vh-310px)] flex flex-col justify-center items-center relative">
+      {/* <div className="container  mx-auto px-7 lg:px-5 xl:px-7"></div> */}
+      <Hero/>
+      <img className="absolute bottom-0 w-full" src={wave} alt="" />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,58 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        animatedgradient: {
+          "0%": { backgroundPosition: "0% 50%" },
+          "50%": { backgroundPosition: "100% 50%" },
+          "100%": { backgroundPosition: "0% 50%" },
+        },
+      },
+      backgroundSize: {
+        "300%": "300%",
+      },
+      animation: {
+        gradient: "animatedgradient 6s ease infinite alternate",
+      },
+    },
   },
+  // eslint-disable-next-line no-undef
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["synthwave","light"],
+    themes: [
+      "light",
+      "dark",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "night",
+      "coffee",
+      "winter",
+      "dim",
+      "nord",
+      "sunset",
+    ],
   },
 };
