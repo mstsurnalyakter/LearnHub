ce3c59709236dc752160e5380b6a512fbfe23976
mstsurnalyakter <surnalyms@gmail.com>
2024-03-21 18:32:56 +0600

    added images

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App
