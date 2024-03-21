a753f5844b0d3671faad26b5690cea5896bea1ae
mstsurnalyakter <surnalyms@gmail.com>
2024-03-22 00:17:01 +0600

    footer section added

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router";
 import NavBar from "../components/NavBar"
+import Footer from "../components/Footer";
 
 
 const MainLayout = () => {
@@ -8,9 +9,10 @@ const MainLayout = () => {
       <div className="h-24">
         <NavBar />
       </div>
-      <div className="min-h-[calc(100vh-117px)]">
+      <div className="min-h-[calc(100vh-310px)]">
         <Outlet />
       </div>
+      <Footer/>
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div className="">
+    <div className="container  mx-auto px-7 lg:px-5 xl:px-7 min-h-[calc(100vh-310px)]">
       <h1>Home</h1>
     </div>
   );
